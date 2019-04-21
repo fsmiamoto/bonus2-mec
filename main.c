892ce75c0335d35954b784d988a0ee2dfe5030fb
joaozanlorensi <joaozanlorensi@gmail.com>
2019-04-21 18:41:04 -0300

    Adicionada a excentricidade e corrigidos alguns formatos

--- main.c
+++ main.c
@@ -66,7 +66,7 @@ void apresentaMenu(){
     printf("Selecione o planeta desejado: \n");
     printf("1) Mercurio\n2) Venus\n3) Terra\n4) Marte\n5) Jupiter\n");
 
-    scanf("%d",&escolha);
+    scanf("%d", &escolha);
 
     switch(escolha){
         case MERCURIO:
@@ -103,28 +103,39 @@ void apresentaMenu(){
 void realizaCalculos(){
     alturaDeBurnout = 0.29 * raio; // Altura na qual a gravidade se reduz a 60%
 
-    velocidadeDeEscape = sqrt((2*gravidade*pow(raio,2)) / (alturaDeBurnout));
-    velocidadeCircular = velocidadeDeEscape / sqrt(2);
+    velocidadeDeEscape = sqrt((2*gravidade*pow(raio,2.0)) / (alturaDeBurnout));
+    velocidadeCircular = velocidadeDeEscape / sqrt(2.0);
 
-    '''
+    /* 
         Numa trajetoria eliptica, a velocidade de lancamento deve estar entre as velocidades circular e de escape.
-        Para a simulacao, utilizamos a velocidade de lancamento como a media aritmetica dessas velocidades:
-    '''
+        Assim, para a simulacao, utilizamos a velocidade de lancamento como a media aritmetica dessas velocidades:
+    */
+
     velocidadeInicial = 0.5 * (velocidadeCircular + velocidadeDeEscape); 
 
     momentoPorMassa = velocidadeInicial * alturaDeBurnout;
 
-    double GM_H2 = (pow(gravidade,2) * pow(raio, 4)) / pow(momentoPorMassa, 2); // Valor de GM / h^2
+    double GM_H2 = (pow(gravidade,2.0) * pow(raio, 4.0)) / pow(momentoPorMassa, 2.0); // Valor de GM / h^2
 
-    C = (1.0 / alturaDeBurnout) - GM_H2 ; // Constante C
+    C = (1.0 / (alturaDeBurnout)) - GM_H2 ; // Constante C
 
     altitudeMaxima = 1.0 / ( GM_H2 - C);
 
     velocidadeNaAltitudeMaxima = momentoPorMassa / altitudeMaxima;
+
+    double a = 0.5 * (alturaDeBurnout + altitudeMaxima);
+    double b = sqrt(alturaDeBurnout * altitudeMaxima);
+
+    excentricidade = a / b;
+
+    periodoDaNave = 2 * pi() * a * b / momentoPorMassa
 }
 
 void apresentaResultados(){
     printf("Velocidade de escape: %.2f m/s\n", velocidadeDeEscape);
     printf("Altitude Maxima: %.2f m\n", altitudeMaxima);
-    printf("Velocidade na altitude maxima: %.2f m/s\n\n", velocidadeNaAltitudeMaxima);
+    printf("Velocidade na altitude maxima: %.2f m/s\n", velocidadeNaAltitudeMaxima);
+    printf("Periodo de orbita da nave: %.2f s\n", periodoDaNave);
+    printf("Valor da excentricidade: %.2f\n\n", excentricidade);
+
 }
